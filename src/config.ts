2f716d32ac6116d30dd46832b303f2549fef5355
Warm, Marco <Marco.Warm@birkenstock.com>
2020-03-13 12:54:31 +0100

    working probe with target

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const server_port=60000;
-export const lan_server_url='http://172.25.26.12/export.csv';
+export const server_port=9688;
+export const lan_server_url='http://172.25.26.12/export.csv?lang=english';
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {lan_server_url, server_port} from './config';
 import {Curl} from "node-libcurl";
 import {parse} from 'papaparse';
+import * as PromClient from 'prom-client';
 
 const app = express();
 
@@ -45,6 +46,78 @@ app.get('/metrics/parsed', (req, res) => {
     });
 
     curl.on('error', curl.close.bind(curl));
+    curl.perform();
+});
+
+app.get('/probe', (req, res) => {
+    // create new Curl Object
+    const curl = new Curl();
+
+    // set Curl options
+    curl.setOpt('URL', lan_server_url);
+    curl.setOpt('FOLLOWLOCATION', true);
+
+    // on Curl end result handle csv data in result
+    curl.on(
+        'end',
+        (statusCode, data, headers) => {
+        if (statusCode !== 200) {
+            // send any non success directly to client
+            res.sendStatus(statusCode)
+        } else {
+            // convert csv to array
+            let arrayData = parse(
+                data.toString(),
+                {
+                    delimiter: ';',
+                    skipEmptyLines: true
+                }).data;
+
+            // find index of electricity meter.
+            // PID of meter is taken from query parameter target e.g. /probe?target=5I8P1265
+            const targetIndex = (arrayData[2] as string[]).findIndex( (s) => s.trim() === req.query.target );
+            if (targetIndex < 0) {
+                // target not found? tell the client!
+                res.sendStatus(404)
+            } else {
+                // everything seems fine
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+
+                let result: string[] = [];
+
+                arrayData.forEach(
+                    (record, index) => {
+                        /*switch (record[0].trim()) {
+                            case 'Frequenz':
+                                result.push('# HELP electricity_meter_frequency Frequency in ' + record[1]);
+                                result.push('# TYPE electricity_meter_frequency gauge');
+                                result.push('electricity_meter_frequency ' +
+                                    parseFloat((record[targetIndex] as string)
+                                        .replace(',','.')))
+                                    .toFixed(2);
+                                break;
+                            default: break;
+
+                        }*/
+
+                        if (index > 2) {
+                            let varName = record[0].trim().replace(/[^a-zA-Z0-9]/g, '_').toLowerCase();
+                            result.push('# HELP ' + varName + record[0] + record[1]);
+                            result.push('# TYPE ' + varName + record[0] + ' gauge');
+                            result.push(varName + ' ' + record[2].replace(',', '.').trim());
+                        }
+                    }
+                );
+
+                res.send(result.join('\n'));
+            }
+        }
+    });
+
+    // Bei Curl Fehler, Verbindung trennen
+    curl.on('error', curl.close.bind(curl));
+    // Curl Request ausführen
     curl.perform();
 });
 
