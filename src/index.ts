d2a1b5ec6e65feea19ffd25acba6fb9d07d71fd1
Warm, Marco <Marco.Warm@birkenstock.com>
2020-03-13 14:52:20 +0100

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,8 @@ import express from 'express';
 import {lan_server_url, server_port} from './config';
 import {Curl} from "node-libcurl";
 import {parse} from 'papaparse';
-import * as PromClient from 'prom-client';
 
 const app = express();
-
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.send('Hello world!');
-});
 
 app.get('/metrics/raw', (req, res) => {
     const curl = new Curl();
@@ -61,50 +54,62 @@ app.get('/probe', (req, res) => {
     curl.on(
         'end',
         (statusCode, data, headers) => {
-        if (statusCode !== 200) {
-            // send any non success directly to client
-            res.sendStatus(statusCode)
-        } else {
-            // convert csv to array
-            let arrayData = parse(
-                data.toString(),
-                {
-                    delimiter: ';',
-                    skipEmptyLines: true
-                }).data;
+            let result: string[] = [];
 
-            // find index of electricity meter.
-            // PID of meter is taken from query parameter target e.g. /probe?target=5I8P1265
-            const targetIndex = (arrayData[2] as string[]).findIndex( (s) => s.trim() === req.query.target );
-            if (targetIndex < 0) {
-                // target not found? tell the client!
-                res.sendStatus(404)
+            result.push('# HELP modbus_em_http_status_code Displays wheher or not the probe was a success');
+            result.push('# TYPE modbus_em_http_status_code gauge');
+            result.push('modbus_em_http_status_code ' + statusCode);
+
+            if (statusCode !== 200) {
+                result.push('# HELP modbus_em_success Displays wheher or not the probe was a success');
+                result.push('# TYPE modbus_em_success gauge');
+                result.push('modbus_em_success 0');
             } else {
-                // everything seems fine
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
+                // convert csv to array
+                let arrayData = parse(
+                    data.toString(),
+                    {
+                        delimiter: ';',
+                        skipEmptyLines: true
+                    }).data;
 
-                let result: string[] = [];
+                // find index of electricity meter.
+                // PID of meter is taken from query parameter target e.g. /probe?target=5I8P1265
+                const targetIndex = (arrayData[2] as string[]).findIndex( (s) => s.trim() === req.query.target);
+                if (targetIndex < 0) {
+                    // target not found? tell the client!
+                    res.sendStatus(404)
+                } else {
+                    // everything seems fine
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/plain');
 
-                arrayData.forEach(
-                    (record, index) => {
-                        if (index > 2) {
-                            let varName = record[0]
-                                .trim()
-                                .replace(/[^a-zA-Z0-9]/g, '_')
-                                .replace(/__/g, '_')
-                                .toLowerCase();
-                            result.push('# HELP ' + varName + record[0].trim() + ' ' + record[1].trim());
-                            result.push('# TYPE ' + varName + record[0].trim() + ' gauge');
-                            result.push(varName + ' ' + record[2].replace(',', '.').trim());
+                    arrayData.forEach(
+                        (record, index) => {
+                            if (index > 2) {
+                                let varName = 'modbus_em_' + record[0]
+                                    .trim()
+                                    .replace(/[^a-zA-Z0-9]/g, '_')
+                                    .replace(/__/g, '_')
+                                    .toLowerCase();
+
+                                if (!varName.startsWith('modbus_em_factory_alarm_status')) {
+                                    result.push('# HELP ' + varName + ' ' + record[0].trim() + ' ' + record[1].trim());
+                                    result.push('# TYPE ' + varName + ' gauge');
+                                    result.push(varName + ' ' + record[2].replace(',', '.').trim());
+                                }
+                            }
                         }
-                    }
-                );
+                    );
 
-                res.send(result.join('\n'));
+                    result.push('# HELP modbus_em_success Displays wheher or not the probe was a success');
+                    result.push('# TYPE modbus_em_success gauge');
+                    result.push('modbus_em_success 1');
+
+                    res.send(result.join('\n'));
+                }
             }
-        }
-    });
+        });
 
     // Bei Curl Fehler, Verbindung trennen
     curl.on('error', curl.close.bind(curl));
