e86cfbd0aa66579cc6fafdbddee317998da966cd
Warm, Marco <Marco.Warm@birkenstock.com>
2020-03-17 11:43:38 +0100

    configure script

--- src/config.ts
+++
@@ -1,2 +0,0 @@
-export const server_port=9688;
-export const lan_server_url='http://172.25.26.12/export.csv?lang=english';
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,22 @@
 import express from 'express';
-import {lan_server_url, server_port} from './config';
+import {lanServerHost, serverPort} from './config';
 import {Curl} from "node-libcurl";
 import {parse} from 'papaparse';
 
 const app = express();
-
-app.get('/metrics/raw', (req, res) => {
-    const curl = new Curl();
-
-    curl.setOpt('URL', lan_server_url);
-    curl.setOpt('FOLLOWLOCATION', true);
-
-    curl.on('end', function (statusCode, data, headers) {
-        res.statusCode = (headers[0] as any).result.code;
-        res.send(data);
-    });
-
-    curl.on('error', curl.close.bind(curl));
-    curl.perform();
-});
-
-app.get('/metrics/parsed', (req, res) => {
-    const curl = new Curl();
-
-    curl.setOpt('URL', lan_server_url);
-    curl.setOpt('FOLLOWLOCATION', true);
-
-    curl.on('end', function (statusCode, data, headers) {
-        res.statusCode = (headers[0] as any).result.code;
-        res.setHeader('Content-Type', 'application/json')
-        const arrayData = parse(
-            data.toString(),
-            {
-                delimiter: ';',
-                skipEmptyLines: true
-            }).data;
-        res.send(arrayData);
-    });
-
-    curl.on('error', curl.close.bind(curl));
-    curl.perform();
-});
 
 app.get('/probe', (req, res) => {
     // create new Curl Object
     const curl = new Curl();
 
     // set Curl options
-    curl.setOpt('URL', lan_server_url);
+    curl.setOpt('URL', lanServerHost + '/export.csv?lang=english');
     curl.setOpt('FOLLOWLOCATION', true);
 
     // on Curl end result handle csv data in result
     curl.on(
         'end',
-        (statusCode, data, headers) => {
+        (statusCode, data) => {
             let result: string[] = [];
 
             result.push('# HELP modbus_em_http_status_code Displays wheher or not the probe was a success');
@@ -118,6 +81,6 @@ app.get('/probe', (req, res) => {
 });
 
 // start the Express server
-app.listen(server_port, () => {
-    console.log(`server started at http://localhost:${server_port}`);
+app.listen(serverPort, () => {
+    console.log(`server started at http://localhost:${serverPort}`);
 });
